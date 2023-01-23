fb3a24b0ff8ad5d8abba236a8d6b3522a7e2436d
cuttleman <silluat11@gmail.com>
2023-01-23 11:49:15 +0900

    add whitelist state, round state

--- contracts/CarOil.sol
+++ contracts/CarOil.sol
@@ -20,16 +20,25 @@ contract CarOil is Ownable {
 
     ClaimHistory[] claimHistories;
     Contributor[] contributors;
+
+    uint32 round;
+    // erc20 address => true/false
+    mapping (address => bool) whitelist;
     
     event Deposit(address from, address to, uint amount, uint when);
     event Claim(address from, address to, uint amount, uint when);
+    event Withdraw(address to, uint amount, uint when);
     event ValueReceived(address from, uint amount, uint when);
+    event SetWhitelist(address token, uint when);
 
-    function _getErc20BalanceOf(address _token) internal view returns (uint256){
-      return IERC20(_token).balanceOf(address(this));
+    constructor() Ownable() {
+      round = 1;
     }
 
+    // deposit erc20 token
     function deposit(address _token, uint256 _amount) public {
+      require(whitelist[_token], "That token is a disallowed token.");
+
       IERC20(_token).transferFrom(msg.sender, address(this), _amount);
 
       contributors.push();
@@ -40,9 +49,11 @@ contract CarOil is Ownable {
       emit Deposit(msg.sender, address(this), _amount, block.timestamp); 
     }
 
-
+    // withdraw erc20 token
     function claim(address _token, address _targetUser) public onlyOwner {
-      uint256 carOilBalance = _getErc20BalanceOf(_token);
+      require(whitelist[_token], "That token is a disallowed token.");
+
+      uint256 carOilBalance = getErc20Balance(_token);
 
       IERC20 erc20 = IERC20(_token);
       uint256 allowance = erc20.allowance(address(this), address(this));
@@ -60,27 +71,58 @@ contract CarOil is Ownable {
       claimHistories[newIndex].contributors = contributors;
 
       delete contributors;
+
+      round++;
        
       emit Claim(address(this), _targetUser ,carOilBalance, block.timestamp);
     }
 
+    // withdraw native token
     function withdraw(address payable _to, uint256 _amount) public onlyOwner {
       (bool sent,) = _to.call{value: _amount}("");
+
       require(sent, "Failed to send BNB");
+
+      emit Withdraw(_to, _amount, block.timestamp);
     }
 
-    function history() external view returns (ClaimHistory[] memory) {
+    function setWhitelist(address _token) public onlyOwner {
+      require(whitelist[_token], "already added.");
+      
+      whitelist[_token] = true;
+
+      emit SetWhitelist(_token, block.timestamp);
+    }
+
+    function checkWhitelist(address _token) public view returns (bool){
+      return whitelist[_token];
+    }
+
+    // view claim history
+    function getHistory() external view returns (ClaimHistory[] memory) {
       return claimHistories;
     }
 
-    function contributor() external view returns (Contributor[] memory) {
+    // view contributors of current round
+    function getContributor() external view returns (Contributor[] memory) {
       return contributors;
     }
 
-    function nativeBalance() external view returns (uint256) {
+    // view current round
+    function getCurrentRound() external view returns (uint32) {
+      return round;
+    }
+
+    // view balance of native token
+    function getNativeBalance() external view returns (uint256) {
       return address(this).balance;
     }
 
+    function getErc20Balance(address _token) public view returns (uint256){
+      return IERC20(_token).balanceOf(address(this));
+    }
+
+    // native receiver
     receive() external payable {
       emit ValueReceived(msg.sender, msg.value, block.timestamp);
     }
